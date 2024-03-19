c06529a59a878a37dbe6feded419e6ecc58c3e6f
nwokedi <65553824+nwokedi@users.noreply.github.com>
2024-03-19 20:16:29 -0230

    first commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to QuizCraft!</h1>
+      <p>Test your knowledge and learn something new every day!</p>
+    </div>
+  );
+}
+
+export default HomePage;
